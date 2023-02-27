6c5fb5da3a2ce04d0f0e195e3276d801fcecd87e
Hisham <hisham0502@gmail.com>
2023-02-27 19:24:32 +0530

    Program 4 CDLL code updated in Finals Folder

--- Finals/prg4.c
+++ Finals/prg4.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int size=0;
+
 typedef struct Node{
     struct Node* next;
     struct Node* prev;
@@ -9,33 +11,39 @@ typedef struct Node{
 
 void display(Node* head)
 {
-    if(head->data==0) {
+    if(size==0) {
         printf("Empty DLL\n"); return;
     }
-    Node* temp=head->next;
-    while(temp!=head){
+    Node* temp=head;
+    do{
         printf("%d ", temp->data);
         temp=temp->next;
-    }
+    }while(temp!=head);
     printf("\n");
 }
 
 Node* deleteByKey(Node* head, int k){
-    Node* temp= head->next;
-
-    while(temp!=head)
+    Node* temp= head;
+    if(temp==NULL) {
+        printf("LL Empty. Deletion not possible\n");
+        return NULL;
+    }
+  do
     {
         if(temp->data==k){
             temp->prev->next=temp->next;
-            temp->next=temp->prev;
+            temp->next->prev=temp->prev;
             printf("Element %d deleted\n", temp->data);
+            if(head==temp) head=temp->next;
             free(temp);
+            size--;
+            if(size==0) head=NULL;
             display(head);
             return head;
         }
         temp=temp->next;
-    }
-    printf("Element %d not found", k);
+    } while(temp!=head);
+    printf("Element %d not found\n", k);
     return head;
     
 }
@@ -43,53 +51,67 @@ Node* deleteByKey(Node* head, int k){
 
 void searchByPos(Node* head, int pos){
 
-    Node* temp= head->next;
+    Node* temp= head;
+    if(temp==NULL) {
+        printf("LL Empty\n");
+        return;
+    }
     int i=0;
-    while(temp!=head)
+    if(pos>size && pos<1){
+         printf("Invalid position. Size is %d and position is %d\n",size,pos); return;
+    }
+    do
     {
         i++;
-        if(head->data==i){
-            printf("Element %d found at position %d\n", temp->data, head->data);
+        if(pos==i){
+            printf("Element %d found at position %d\n", temp->data, pos);
             display(head);
             return;
         }
         temp=temp->next;
-    }
-    printf("Invalid position. Size is %d and position is %d\n",head->data,pos);
+    }while(temp!=head);
 }
 
 Node* insertByOrder(Node* head, int data){
        Node* node=(Node *)malloc(sizeof(Node));
        node->data=data;
-       node->next=head;
-       node->prev=head;
 
-       Node* temp=head->next;
-       while(temp->next!=head) temp=temp->next;
-       temp->next=node;
-       node->prev=temp;
-       head->data++;
-
-    printf("Before Insertion: \n");
+       printf("Before Insertion: \n");
     display(head);
-    
-    //Ordering
-    Node* temp=head->next;
-    Node* temp2=NULL;
-    while(temp->next!=head)
+
+       if(head==NULL){
+        node->next=node;
+        node->prev=node;
+        head=node;
+       }
+
+    if(head!=NULL)
     {
+        Node* tail= head->prev;
+       node->next=tail->next;
+       node->prev=tail;
+       tail->next=node;
+       head->prev=node;
+    }
+       
+       size++;
+
+    Node* temp=head;
+    Node* temp2=NULL;
+    
+    do{
         temp2= temp->next;
         
-        while(temp2!=head){
+       do{
             if(temp2->data<temp->data){
                 int tempor= temp2->data;
                 temp2->data=temp->data;
                 temp->data=tempor;
             }
             temp2=temp2->next;
-        }
+        } while(temp2!=head);
         temp=temp->next;
-    }
+    }while(temp->next!=head);
     printf("After insertion: \n");
     display(head);
        return head;
@@ -99,14 +121,12 @@ Node* insertByOrder(Node* head, int data){
 void main()
 {
     int ch,k,pos;
-    Node* head= (Node *)malloc(sizeof(Node));
-    head->data=0;
-    head->prev=head->next=head;
+    Node* head=NULL;
     while(1)
     {
 
 printf("0.Exit\n1.Insert By Order\n2.Delete By Key\n3.Search By Position\n");
-        printf("\n-----\nEnter you choice: ");
+        printf("\n-----\nEnter your choice: ");
         scanf("%d",&ch);
         switch(ch){
             case 0: exit(0);
@@ -118,9 +138,9 @@ printf("0.Exit\n1.Insert By Order\n2.Delete By Key\n3.Search By Position\n");
             break;
 
             case 2:
-            printf("Enter pos to delete\n");
-            scanf("%d",&pos);
-            head=deleteByKey(head, pos);
+            printf("Enter key to be deleted\n");
+            scanf("%d",&k);
+            head=deleteByKey(head, k);
             break;
 
             case 3:
